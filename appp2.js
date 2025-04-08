882f5ad2b62c49fc38085f234dcec314e828372e
JabrailMataev <mataevdhabrail200@gmail.com>
2025-04-08 12:25:09 +0300

    Add files via upload

---
+++ appp2.js
@@ -0,0 +1,13 @@
+function countEvens(numbers){
+for(let i=0; i < numbers.length; i++){
+    let number = numbers[i]
+if(number % 2 === 0){
+    console.log(number)
+}
+else{}
+}
+}
+countEvens([1,2,3,4,5,6,7,10])
+
+
+
